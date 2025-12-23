76871705a1e5085f513250443ca5c5ba6c15185a
PanuwitPond <157445903+PanuwitPond@users.noreply.github.com>
2025-12-23 17:28:50 +0700

    save1

--- frontend/src/pages/mroi/index.ts
+++ frontend/src/pages/mroi/index.ts
@@ -1,5 +1,4 @@
 export { default as DevicesPage } from './DevicesPage';
-export { default as MroiDashboard } from './MroiDashboard';
 export { default as MroiEmbedPage } from './MroiEmbedPage';
 export { default as RoisPage } from './RoisPage';
 export { default as SchedulesPage } from './SchedulesPage';
